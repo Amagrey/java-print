22a781c0ae2a1582ab3137ef67133a1675cbb12a
Amagrey <105565896+Amagrey@users.noreply.github.com>
2022-06-24 22:05:06 -0700

    Add files via upload

---
+++ Java-text/script.js
@@ -0,0 +1,6 @@
+console.log("Name: Ndubueze Amazing-Grace \n\n Height: 5ft11 \n\n Country: Nigeria")
+alert ("Name: Ndubueze Amazing-Grace \n\n Height: 5ft11 \n\n Country: Nigeria")
+
+
+
+document.write (' Hello! I am Ndubueze Amazing-Grace <p> I am 5ft11</p> <p> And I am from Nigeria </p>')
